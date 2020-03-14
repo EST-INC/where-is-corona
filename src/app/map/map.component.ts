dcc8593ae8e503b0a61dd036f347747dc200af15
Lelental <p.lelental@gmail.com>
2020-03-14 12:27:02 +0100

    Removed not appropriate marker icon

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -18,7 +18,7 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
     this.mymap = L.map('mapid', {
       center: [this.latitude, this.longitude],
-      zoom: 20
+      zoom: 15
     });
     // inits map layer from Open Street Map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -29,24 +29,7 @@ export class MapComponent implements OnInit {
     // show the scale bar on the lower left corner
     L.control.scale().addTo(this.mymap);
 
-    // show a marker on the map
-    L.marker({ lat: this.latitude, lon: this.longitude }, { icon: this.getMarkerIcon() })
-      .bindPopup('Jaskinia gargamela')
-      .addTo(this.mymap);
-    
     // adds poland boundaries to map
     L.geoJSON(polandBoundaries).addTo(this.mymap);
-  }
-
-  private getMarkerIcon() {
-    return L.icon({
-      iconUrl: '../assets/icon.svg',
-      iconSize: [38, 95],
-      iconAnchor: [22, 94],
-      popupAnchor: [-3, -76],
-      shadowUrl: '../assets/icon-shadow.png',
-      shadowSize: [50, 45],
-      shadowAnchor: [10, 67]
-    });
   }
 }
